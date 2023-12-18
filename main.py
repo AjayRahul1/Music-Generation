b419d493485b839b48acf194653028eb989311d1
AjayRahul1 <p.ajayrahul02@gmail.com>
2023-12-19 00:39:09 +0530

    A lot of changes. Click ... (Description)
    Changes:
    1.Librosa, python-multipart, matplotlib==3.7 added into requirements.txt
    2. Home page button now posts request to /generate-music
    3. post request takes prompt through Form
    4. Temporarily took an audio file generated and creates waveform.
    5. Later I will change from same audio file to generated audio file.
    6. Plotted the Waveform using matplotlib
    7. Converted the plot into image and returned as a HTML response.
    8. Later, I will change this HTML response into AJAX request to update on same page dynamically.
    9. HTMX is added into index.html and name attributes for few elements changed.
    10. audio file added to gitignore
    11. Audio waveform request has to be changed into AJAX yet to display it on same HTML page instead of new

--- main.py
+++ main.py
@@ -1,9 +1,49 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+import matplotlib.pyplot as plt, io, base64
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+  context = {"request" : request}
+  return templates.TemplateResponse('index.html', context=context)
+
+def conv_to_base64(fig):
+  img_stream = io.BytesIO()
+  fig.savefig(img_stream, format="png")
+  img_stream.seek(0)
+  img_data = base64.b64encode(img_stream.read()).decode("utf-8")
+  return img_data
+
+@app.post('/generate-music', response_class=HTMLResponse)
+async def generate_music(musicPrompt : str=Form(...)):
+  import librosa
+  # Load the audio file
+  file_path = 'musicgen_out.wav'
+  y, sr = librosa.load(file_path)
+
+  # Generate the waveform plot without axis labels
+  plt.figure(figsize=(10, 2))
+  librosa.display.waveshow(y, sr=sr)
+
+  # Remove axis labels
+  plt.axis('off')
+  audio_waverform = conv_to_base64(plt)
+  plt.close()
+
+  html_content = f"""
+  <html>
+    <body>
+      <div class="border rounded mb-2 overflow-hidden">
+        <img src="data:image/png;base64,{audio_waverform}" alt="Waveform" style="width: 100%; max-height: min-content;">
+        <h2><strong>{musicPrompt}</strong></h2>
+      </div>
+    </body>
+  </html>
+  """
+  return HTMLResponse(content=html_content, status_code=200)
