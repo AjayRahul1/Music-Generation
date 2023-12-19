4837841ae0f7e0bb8138d9a6dc9b139882608f20
AjayRahul1 <p.ajayrahul02@gmail.com>
2023-12-19 18:10:16 +0530

    Feature: Audio can be played and downloaded
    The audio is static one. It will generated using Deep Generative Model later

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 import matplotlib.pyplot as plt, io, base64
 
 app = FastAPI()
@@ -20,8 +20,7 @@ def conv_to_base64(fig):
   img_data = base64.b64encode(img_stream.read()).decode("utf-8")
   return img_data
 
-@app.post('/generate-music', response_class=HTMLResponse)
-async def generate_music(musicPrompt : str=Form(...)):
+def plot_waveform():
   import librosa
   # Load the audio file
   file_path = 'musicgen_out.wav'
@@ -33,17 +32,35 @@ async def generate_music(musicPrompt : str=Form(...)):
 
   # Remove axis labels
   plt.axis('off')
-  audio_waverform = conv_to_base64(plt)
+  audio_waveform = conv_to_base64(plt)
   plt.close()
+  return audio_waveform
 
+@app.post('/generate-music', response_class=HTMLResponse)
+async def generate_music(musicPrompt : str=Form(...)):
+  audio_waveform = plot_waveform()
   html_content = f"""
-  <html>
-    <body>
-      <div class="border rounded mb-2 overflow-hidden">
-        <img src="data:image/png;base64,{audio_waverform}" alt="Waveform" style="width: 100%; max-height: min-content;">
-        <h2><strong>{musicPrompt}</strong></h2>
-      </div>
-    </body>
-  </html>
+  <img src="data:image/png;base64,{audio_waveform}" alt="Waveform" style="width: 100%; max-height: min-content;">
+  <audio id="myAudio" src="/audio" preload="auto"></audio>
+  <div class="text-center">
+    <button class="btn btn-outline-success me-1" onclick="playMusic()">Play</button>
+    <a href="/audio" download="MusicGen - Define your sound"><button class="btn btn-outline-success ms-1">Download</button></a>
+  </div>
+  <script>
+    function playMusic() {{
+      var audio = document.getElementById('myAudio');
+      if (audio.paused) {{
+        audio.play();
+      }} else {{
+        audio.pause();
+      }}
+    }}
+  </script>
   """
   return HTMLResponse(content=html_content, status_code=200)
+
+# Endpoint to serve the audio file
+@app.get("/audio")
+async def get_audio_file():
+  audio_path = "musicgen_out.wav"
+  return FileResponse(audio_path)
