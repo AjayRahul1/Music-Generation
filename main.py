37b61d02881bdddb7a3ffaa106d5733e6e4bd67b
AjayRahul1 <p.ajayrahul02@gmail.com>
2024-02-27 20:27:18 +0530

    custom play, download btns
    1. variables for colors and gradients in CSS.
    2. fixed a bug where it shows loading animation when the textArea is empty.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
 import matplotlib.pyplot as plt, io, base64
 import librosa
+from transformers import pipeline
+import scipy
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,52 +13,58 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
-  context = {"request" : request}
-  return templates.TemplateResponse('index.html', context=context)
+  return templates.TemplateResponse('index.html', context={"request" : request})
 
-def conv_to_base64(fig):
+def conv_to_base64(fig: plt) -> str:
+  """Converts a plot into image varibale in Base64 Format such that it can displayed on the website.
+  :param fig: plot variable.
+  :return: Image in string base64 format."""
   img_stream = io.BytesIO()
   fig.savefig(img_stream, format="png")
   img_stream.seek(0)
   img_data = base64.b64encode(img_stream.read()).decode("utf-8")
   return img_data
 
-def generate_audio(musicPrompt, audioLength):
-  from transformers import pipeline
-  import scipy
+def generate_audio(musicPrompt: str, audioLength: int):
+  """Generates the audio for the prompt entered."""
   synthesiser = pipeline("text-to-audio", "facebook/musicgen-small")
   max_length = int(51.2 * audioLength) # for 15 seconds (768/51.2 = 15)
   music = synthesiser(musicPrompt, forward_params={"do_sample": True, "max_length": max_length})
   scipy.io.wavfile.write("musicgen_out.wav", rate=music["sampling_rate"], data=music["audio"])
 
-def plot_waveform(musicPrompt, audioLength):
+def plot_waveform(musicPrompt: str, audioLength: int) -> str:
+  """Generates the waveplot for the audio that is generated."""
   # Load the audio file
   file_path = 'musicgen_out.wav'
   y, sr = librosa.load(file_path)
 
+  # Create a variable for the plot
+  fig, ax = plt.subplots(figsize=(8, 2))
   # Generate the waveform plot without axis labels
-  plt.figure(figsize=(8, 2), facecolor='green')
-  plt.title(label=musicPrompt + " - " + str(audioLength) + " seconds")
-  plt.tight_layout()
-  librosa.display.waveshow(y, sr=sr, color='k')
-
+  fig.patch.set_facecolor('#C0CFFA')
+  ax.set_title(label=musicPrompt + " - " + str(audioLength) + " seconds")
+  fig.tight_layout()
+  librosa.display.waveshow(y, sr=sr, color='k', ax=ax)
   # Remove axis labels
-  plt.axis('off')
-  audio_waveform = conv_to_base64(plt)
-  plt.close()
+  ax.axis('off')
+  # Convert the plot into base64 image
+  audio_waveform = conv_to_base64(fig)
+  # Close the plot
+  plt.close(fig)
   return audio_waveform
 
 @app.post('/generate-music', response_class=HTMLResponse)
-async def generate_music(musicPrompt : str=Form(..., title="musicPrompt"), audioLength : int=Form(...)):
-  generate_audio(musicPrompt, audioLength)
+async def generate_music(musicPrompt: str=Form(..., title="musicPrompt"), audioLength: int=Form(...)):
   print("Generating Audio...\nPrompt:", musicPrompt, "\nLength: ", audioLength, " seconds")
-  audio_waveform = plot_waveform(musicPrompt, audioLength)
+  generate_audio(musicPrompt, audioLength)
+  audio_waveform_img = plot_waveform(musicPrompt, audioLength)
+  audio_filepath="musicgen_out"
   html_content = f"""
-  <img class="rounded" src="data:image/png;base64,{audio_waveform}" alt="Waveform" style="width:100%; max-height: max-content;">
-  <audio id="myAudio" src="/audio" preload="auto"></audio>
+  <img class="rounded" src="data:image/png;base64,{audio_waveform_img}" alt="Waveform" style="width:100%; max-height: max-content;">
+  <audio id="myAudio" src="/audio/{audio_filepath}" preload="auto"></audio>
   <div class="text-center my-2">
-    <button class="btn btn-success me-1" onclick="playMusic()">Play</button>
-    <a href="/audio" download="{musicPrompt} - MusicGen - Define your sound"><button class="btn btn-success ms-1">Download</button></a>
+    <button class="blueGradientBtn me-1" onclick="playMusic()">Play</button>
+    <a href="/audio" download="{musicPrompt} - AudioYours - Define your sound"><button class="blueGradientBtn ms-1">Download</button></a>
   </div>
   <script>
     function playMusic() {{
@@ -72,7 +80,7 @@ async def generate_music(musicPrompt : str=Form(..., title="musicPrompt"), audio
   return HTMLResponse(content=html_content, status_code=200)
 
 # Endpoint to serve the audio file
-@app.get("/audio")
-async def get_audio_file():
-  audio_path = "musicgen_out.wav"
-  return FileResponse(audio_path)
+@app.get("/audio/{audio_filepath}")
+async def get_audio_file(audio_filepath: str):
+  audio_file_extension = ".wav"
+  return FileResponse(audio_filepath+audio_file_extension)
